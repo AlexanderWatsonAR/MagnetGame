21b6cf3af5488af8ad23d8b6378fa8f4db69656c
Alexander Watson <msnalexwatson@hotmail.co.uk>
2020-09-17 11:04:35 +0100

    Animated melting pots
    Created test scene for melting pots pouring liquid into the mould

---
+++ Assets/Play.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            GetComponent<Animator>().enabled = true;
+            GetComponent<Animator>().Play(0);
+        }
+    }
+}
